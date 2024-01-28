17990e768f56a06c31dcdef3c8538f42c0393afd
Mark Tedra <marktedra29@gmail.com>
2024-01-28 11:47:26 +0800

    Starting Point

---
+++ TedsM/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+const Nav = () => {
+
+  return (
+      <nav className="flex ">
+        <div className="">
+            <a className="text-white px-10 font-medium text-xl"href="">Profile</a>
+            <a className="text-white px-10 font-medium text-xl"href="">Timeline</a>
+            <a className="text-white px-10 font-medium text-xl"href="">Skill</a>
+            <a className="text-white px-10 font-medium text-xl"href="">Contact</a>
+        </div>
+      </nav>
+  )
+}
+
+export default Nav
---
+++ TedsM/src/components/buttons.jsx
@@ -0,0 +1,9 @@
+const buttons = () => {
+  return (
+   <div>
+    <button>Submit</button>
+   </div>
+  )
+}
+
+export default buttons
